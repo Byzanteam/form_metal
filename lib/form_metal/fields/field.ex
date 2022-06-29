3bb331652a9c614712e81a7b8774a99f24ec2527
Phil Chen <06fahchen@gmail.com>
2022-06-29 11:57:11 +0800

    feat: add field prelude

--- lib/form_metal/fields/field.ex
+++ lib/form_metal/fields/field.ex
@@ -8,6 +8,21 @@ defmodule FormMetal.Fields.Field do
   @doc "Make a field change."
   @callback changeset(field, params :: map()) :: Ecto.Changeset.t(field) when field: field()
 
+  @doc "The field prelude that adds preset configuration to the field."
+  @spec prelude() :: Macro.t()
+  def prelude do
+    quote do
+      use Ecto.Schema
+
+      @primary_key false
+      @timestamps_opts [type: :naive_datetime_usec]
+
+      @before_compile FormMetal.Fields.Field
+      @behaviour FormMetal.Fields.Field
+      @defoverridable FormMetal.Fields.Field
+    end
+  end
+
   defmacro __before_compile__(_env) do
     quote do
       unless Module.defines_type?(__MODULE__, {:t, 0}) do
