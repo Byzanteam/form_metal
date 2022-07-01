301dac9efeb1352027a6909b36b9c10e10e3d266
Phil Chen <06fahchen@gmail.com>
2022-07-01 10:56:17 +0800

    feat: add the is operator filter builder

---
+++ lib/form_metal/filters/builders/is_builder.ex
@@ -0,0 +1,31 @@
+defmodule FormMetal.Filters.Builders.IsBuilder do
+  @moduledoc """
+  Build a `is` operator for a filter.
+  """
+
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      value_ecto_type = Keyword.fetch!(opts, :value_ecto_type)
+      value_type = Keyword.fetch!(opts, :value_type)
+
+      use FormMetal.Filters.Filter
+
+      embedded_schema do
+        field :source, value_ecto_type
+        field :value, value_ecto_type
+      end
+
+      @type t() :: %__MODULE__{
+              source: maybe(unquote(value_type)),
+              value: maybe(unquote(value_type))
+            }
+
+      defimpl FormMetal.Filters.Testers.InMemory do
+        @spec test(%@for{}) :: boolean()
+        def test(filter) do
+          filter.source === filter.value
+        end
+      end
+    end
+  end
+end
