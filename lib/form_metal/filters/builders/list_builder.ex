66dd43c3ea11cb3ec8ee531326d75f13483fee67
Phil Chen <06fahchen@gmail.com>
2022-07-04 12:13:13 +0800

    refactor: use after_compile hook to create list filters

--- lib/form_metal/filters/builders/list_builder.ex
+++ lib/form_metal/filters/builders/list_builder.ex
@@ -5,88 +5,112 @@ defmodule FormMetal.Filters.Builders.ListBuilder do
   """
 
   defmacro __using__(opts) do
-    operator = Keyword.fetch!(opts, :operator)
-    singular_filters = Keyword.fetch!(opts, :singular_filters)
+    opts
+    |> Keyword.fetch!(:operators)
+    |> Macro.expand(__CALLER__)
+    |> Enum.map(fn
+      :all ->
+        Module.put_attribute(__CALLER__.module, :after_compile, {__MODULE__, :build_all_operator})
 
-    Enum.flat_map(singular_filters, fn singular_filter ->
-      singular_filter = Macro.expand(singular_filter, __CALLER__)
-
-      Code.ensure_compiled!(singular_filter)
-
-      name = singular_filter |> Module.split() |> List.last() |> String.to_existing_atom()
-      # credo:disable-for-next-line Credo.Check.Warning.UnsafeToAtom
-      module_name = Module.concat(__CALLER__.module, name)
-
-      fields =
-        Enum.map(singular_filter.__schema__(:fields), fn field ->
-          {field, singular_filter.__schema__(:type, field)}
-        end)
-
-      t_typespec =
-        case Code.Typespec.fetch_types(singular_filter) do
-          :error ->
-            raise ArgumentError,
-                  "No type information for the singular filter: `#{inspect(singular_filter)}`"
-
-          {:ok, types} ->
-            types
-            |> Enum.find(&match?({:type, {:t, _meta, []}}, &1))
-            # credo:disable-for-next-line Credo.Check.Refactor.Nesting
-            |> case do
-              nil ->
-                raise ArgumentError,
-                      "No type information for `#{inspect(singular_filter)}.t()` was found or `#{inspect(singular_filter)}.t()` is private."
-
-              {:type, typespec} ->
-                typespec
-            end
-        end
-
-      t_type_def = make_t_type(t_typespec, {singular_filter, module_name})
-
-      module =
-        quote location: :keep,
-              bind_quoted: [
-                module_name: module_name,
-                fields: fields,
-                t_type_def: t_type_def
-              ] do
-          defmodule module_name do
-            use FormMetal.Filters.Filter
-
-            embedded_schema do
-              Enum.each(fields, fn
-                {:source, type} ->
-                  field :source, {:array, type}
-
-                {name, type} ->
-                  field name, type
-              end)
-            end
-
-            @type unquote(t_type_def)
-          end
-        end
-
-      implement =
-        quote location: :keep,
-              bind_quoted: [
-                operator: operator,
-                singular_filter: singular_filter,
-                module_name: module_name
-              ] do
-          defimpl FormMetal.Filters.Testers.InMemory, for: module_name do
-            alias FormMetal.Filters.Builders.InMemoryUtils
-
-            @spec test(%@for{}) :: boolean()
-            def test(filter) do
-              InMemoryUtils.list_test(unquote(operator), filter, unquote(singular_filter))
-            end
-          end
-        end
-
-      [module, implement]
+      :any ->
+        Module.put_attribute(__CALLER__.module, :after_compile, {__MODULE__, :build_any_operator})
     end)
+  end
+
+  @spec build_any_operator(Macro.Env.t(), binary()) :: term()
+  def build_any_operator(env, bytecode) do
+    do_build(:any, env, bytecode)
+  end
+
+  @spec build_all_operator(Macro.Env.t(), binary()) :: term()
+  def build_all_operator(env, bytecode) do
+    do_build(:all, env, bytecode)
+  end
+
+  defp do_build(operator, env, bytecode) when operator in [:all, :any] do
+    singular_filter_module = env.module
+    list_filter_module = build_list_filter_name(env, operator)
+
+    fields =
+      Enum.map(singular_filter_module.__schema__(:fields), fn field ->
+        {field, singular_filter_module.__schema__(:type, field)}
+      end)
+
+    t_type_def =
+      case fetch_t_typespec(bytecode) do
+        {:ok, typespec} ->
+          make_t_type(typespec, {singular_filter_module, list_filter_module})
+
+        :error ->
+          raise ArgumentError,
+                "No type information for `#{inspect(singular_filter_module)}.t()` was found or `#{inspect(singular_filter_module)}.t()` is private."
+      end
+
+    contents =
+      quote location: :keep,
+            bind_quoted: [
+              singular_filter_module: singular_filter_module,
+              list_filter_module: list_filter_module,
+              fields: fields,
+              operator: operator,
+              t_type_def: t_type_def
+            ] do
+        use FormMetal.Filters.Filter
+
+        embedded_schema do
+          Enum.each(fields, fn
+            {:source, type} ->
+              field :source, {:array, type}
+
+            {name, type} ->
+              field name, type
+          end)
+        end
+
+        @type unquote(t_type_def)
+
+        # the in_memory tester implement
+        defimpl FormMetal.Filters.Testers.InMemory, for: list_filter_module do
+          alias FormMetal.Filters.Builders.InMemoryUtils
+
+          @spec test(%@for{}) :: boolean()
+          def test(filter) do
+            InMemoryUtils.list_test(unquote(operator), filter, unquote(singular_filter_module))
+          end
+        end
+      end
+
+    Module.create(list_filter_module, contents, Macro.Env.location(__ENV__))
+  end
+
+  defp build_list_filter_name(env, operator) do
+    names =
+      env.module
+      |> Module.split()
+      |> List.insert_at(-2, "List")
+      |> List.insert_at(-2, operator |> Atom.to_string() |> Macro.camelize())
+
+    # credo:disable-for-next-line Credo.Check.Warning.UnsafeToAtom
+    Module.concat(names)
+  end
+
+  defp fetch_t_typespec(bytecode) do
+    case Code.Typespec.fetch_types(bytecode) do
+      :error ->
+        :error
+
+      {:ok, types} ->
+        types
+        |> Enum.find(&match?({:type, {:t, _meta, []}}, &1))
+        # credo:disable-for-next-line Credo.Check.Refactor.Nesting
+        |> case do
+          nil ->
+            :error
+
+          {:type, typespec} ->
+            {:ok, typespec}
+        end
+    end
   end
 
   defp make_t_type(type_def, {singular_filter_module, list_filter_module}) do
