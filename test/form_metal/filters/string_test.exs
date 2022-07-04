067e9f7f874012651167d7ca20428864a3d7dba2
Phil Chen <06fahchen@gmail.com>
2022-07-04 12:30:22 +0800

    test: list string values

--- test/form_metal/filters/string_test.exs
+++ test/form_metal/filters/string_test.exs
@@ -29,4 +29,68 @@ defmodule FormMetal.Filters.StringTest do
       ])
     end
   end
+
+  describe "InMemory tester for list values" do
+    test "all is" do
+      assert_in_memory_pairs(FormMetal.Filters.String.List.All.Is, [
+        {[["foo"], "foo"], true},
+        {[["foo", "foo"], "foo"], true},
+        {[["foo", "bar"], "foo"], false},
+        {[["foo", nil], "foo"], false},
+        {[[nil], nil], false},
+        {[nil, "foo"], false},
+        {[nil, nil], false}
+      ])
+    end
+
+    test "any is" do
+      assert_in_memory_pairs(FormMetal.Filters.String.List.Any.Is, [
+        {[["foo"], "foo"], true},
+        {[["foo", "foo"], "foo"], true},
+        {[["foo", "bar"], "foo"], true},
+        {[["foo", nil], "foo"], true},
+        {[[nil], nil], false},
+        {[nil, "foo"], false},
+        {[nil, nil], false}
+      ])
+    end
+
+    test "all is_empty" do
+      assert_in_memory_pairs(FormMetal.Filters.String.List.All.IsEmpty, [
+        {[["foo"]], false},
+        {[["foo", nil]], false},
+        {[[nil, nil]], true},
+        {[nil], false}
+      ])
+    end
+
+    test "any is_empty" do
+      assert_in_memory_pairs(FormMetal.Filters.String.List.Any.IsEmpty, [
+        {[["foo"]], false},
+        {[["foo", nil]], true},
+        {[[nil, nil]], true},
+        {[nil], false}
+      ])
+    end
+
+    test "all contains" do
+      assert_in_memory_pairs(FormMetal.Filters.String.List.All.Contains, [
+        {[["foobar"], "foo"], true},
+        {[["foobar", "foo"], "foobar"], false},
+        {[["foo"], nil], false},
+        {[[nil], "foo"], false},
+        {[nil, nil], false}
+      ])
+    end
+
+    test "any contains" do
+      assert_in_memory_pairs(FormMetal.Filters.String.List.Any.Contains, [
+        {[["foobar"], "foo"], true},
+        {[["foobar", "foo"], "foobar"], true},
+        {[["foo"], nil], false},
+        {[[nil], "foo"], false},
+        {[nil, nil], false}
+      ])
+    end
+  end
 end
