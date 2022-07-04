2be641f7308019d3239479527541d4eb0815759e
Phil Chen <06fahchen@gmail.com>
2022-07-04 17:46:45 +0800

    feat: add include and exclude builder

---
+++ lib/form_metal/filters/builders/exclude_builder.ex
@@ -0,0 +1,36 @@
+defmodule FormMetal.Filters.Builders.ExcludeBuilder do
+  @moduledoc """
+  Build a `exclude` operator for a list filter.
+  """
+
+  defmacro __using__(opts) do
+    quote bind_quoted: [opts: opts] do
+      value_ecto_type = Keyword.fetch!(opts, :value_ecto_type)
+      value_type = Keyword.fetch!(opts, :value_type)
+
+      use FormMetal.Filters.Filter
+
+      embedded_schema do
+        field :source, {:array, value_ecto_type}
+        field :value, value_ecto_type
+      end
+
+      @type t() :: %__MODULE__{
+              source: maybe([maybe(unquote(value_type))]),
+              value: maybe(unquote(value_type))
+            }
+
+      defimpl FormMetal.Filters.Testers.InMemory do
+        alias FormMetal.Values.Value
+        import FormMetal.Filters.Builders.InMemoryUtils
+
+        @spec test(%@for{}) :: boolean()
+        def test(filter) do
+          nil_guard(filter) do
+            Value.include?(unquote(value_ecto_type), filter.value, filter.source)
+          end
+        end
+      end
+    end
+  end
+end
--- lib/form_metal/filters/string/list/include.ex
+++ lib/form_metal/filters/string/list/include.ex
@@ -3,27 +3,7 @@ defmodule FormMetal.Filters.String.List.Include do
   The `include` filter for list string values.
   """
 
-  use FormMetal.Filters.Filter
-
-  embedded_schema do
-    field :source, {:array, :string}
-    field :value, :string
-  end
-
-  @type t() :: %__MODULE__{
-          source: maybe([maybe(String.t())]),
-          value: maybe(String.t())
-        }
-
-  defimpl FormMetal.Filters.Testers.InMemory do
-    alias FormMetal.Values.Value
-    import FormMetal.Filters.Builders.InMemoryUtils
-
-    @spec test(%@for{}) :: boolean()
-    def test(filter) do
-      nil_guard(filter) do
-        Value.include?(:string, filter.value, filter.source)
-      end
-    end
-  end
+  use FormMetal.Filters.Builders.IncludeBuilder,
+    value_ecto_type: :string,
+    value_type: quote(do: String.t())
 end
